0c9b8da507a12e06fe8712105ccdd9ad783909d0
Dani5inq <yungwizzy18@gmail.com>
2023-06-27 19:21:11 +0200

    first commit

---
+++ tipcal.js
@@ -0,0 +1,14 @@
+const buttonElement = document.getElementById("calculate");
+const billInputElement = document.getElementById("bill");
+const tipInputElement = document.getElementById("tip");
+const totalSpanElement = document.getElementById("total");
+
+function calculateTotal() {
+  const billValue = billInputElement.value;
+  const tipValue = tipInputElement.value;
+  const totalvalue = Math.floor(billValue * (1 + tipValue / 100));
+
+  totalSpanElement.innerText = totalvalue.toFixed(2);
+}
+
+buttonElement.addEventListener("click", calculateTotal);
